6d7c40c28808dbc7f7b03fef3a04f2775cba7991
akirasoft <akirasoft@gmail.com>
2019-01-03 16:39:55 -0600

    adding files for creating multiple active gates at once

---
+++ terraform-multiple-ags/instance.tf
@@ -0,0 +1,108 @@
+# instance file
+# it contains the aws instance details
+# originally Raffaele Talarico
+# 08/29/2017
+# based on: https://github.com/raffaele-talarico/dynatrace-terraform-aws
+# repurposed for ActiveGate by Michael Villiger
+# 1/2/2019
+
+# for further details about aws_instance available arguments 
+# check out https://www.terraform.io/docs/providers/aws/r/instance.html
+# for further details about Dynatrace ActiveGate install parameters:
+# check out https://www.dynatrace.com/support/help/deploy-dynatrace/activegate/installation/customize-installation-for-activegate/
+# for further details around Dynatrace ActiveGate configuration parameters:
+# check out https://www.dynatrace.com/support/help/deploy-dynatrace/activegate/configuration/configure-activegate/
+
+# This ActiveGate will only be used for executing plugins so ingress will only be allowed on SSH. 
+# Note, best practices typically specify a bastion host for ssh access, we are forgoing that here.
+resource "aws_security_group" "terraformactivegate" {
+  name        = "terraformactivegate"
+  description = "Allow SSH inbound traffic for Dynatrace ActiveGate"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "dynatraceactivegate" {
+
+	count = 2
+	
+	vpc_security_group_ids = ["${aws_security_group.terraformactivegate.id}"]
+	key_name = "${var.AWS_KEYPAIR_NAME}"
+
+	#ami ID and instance type are defined in the vars file 
+  	ami = "${lookup(var.AMIS, var.AWS_REGION)}"
+  	instance_type = "${lookup(var.AWS_INSTANCE_TYPE, var.DYNATRACE_SIZING)}"
+	
+	#EC2 instance root volume size
+	root_block_device {
+        volume_size = "${var.ROOT_VOLUME_SIZE}"
+   	}
+
+	#specify a tag (optional) for the EC2 instance, we utilize email to tag our instance owners
+	tags {
+    	  email = "${var.emailtag}"
+		  name = "pcfactivegate-${count.index}"
+  	}
+	
+# The following block downloads and executes the Dynatrace ActiveGate installer
+# and configures the ActiveGate to run the CloudFoundry plugin and not accept agent traffic
+# && must be used to ensure entire remote-exec block will fail if individual commands fail
+	provisioner "remote-exec" {
+    		inline = [
+      			"sudo wget -O /tmp/activegate.sh \"${element(var.DYNATRACE_DOWNLOAD_URLS, count.index)}\" && cd /tmp/ && sudo /bin/sh /tmp/activegate.sh"
+    		]
+	#the connection block defines the connection params to ssh into the newly created EC2 instance 
+			connection {
+				type = "ssh"
+				user = "ubuntu"
+				private_key = "${var.AWS_PRIVATE_KEY}"
+				timeout = "3m"
+				agent = false
+			}
+  	}
+# the below block loads the persistant custom config that enables the CF plugin and disables the AG handling agent traffic
+# must be put in /tmp because of permissions on /var/lib/dynatrace/gateway/config/
+ 	provisioner "file" {
+    source      = "conf/custom.properties"
+    destination = "/tmp/custom.properties"
+			connection {
+				type = "ssh"
+				user = "ubuntu"
+				private_key = "${var.AWS_PRIVATE_KEY}"
+				timeout = "3m"
+				agent = false
+			}
+  }
+
+# after moving custom config, we need to restart the AG	
+	provisioner "remote-exec" {
+    		inline = [
+      			"sudo mv /tmp/custom.properties /var/lib/dynatrace/gateway/config/custom.properties && sudo chown dtuserag.dtuserag /var/lib/dynatrace/gateway/config/custom.properties && sudo service dynatracegateway forcestop && sudo service dynatracegateway start"
+    		]
+	#the connection block defines the connection params to ssh into the newly created EC2 instance 
+			connection {
+				type = "ssh"
+				user = "ubuntu"
+				private_key = "${var.AWS_PRIVATE_KEY}"
+				timeout = "3m"
+				agent = false
+			}
+  	}	
+}
+
+#a little trick to show the complete url with the public_dns of the created node
+output "public_ip_for_ssh" {
+  value = "${aws_instance.dynatraceactivegate.public_dns}"
+}
---
+++ terraform-multiple-ags/vars.tf
@@ -0,0 +1,92 @@
+# variables file
+# contains the aws access credentials, aws amis per region, aws instance type depending on ActiveGate sizing 
+# the Dynatrace download URL retrieved from Dynatrace UI
+#
+# originally Raffaele Talarico
+# 08/29/2017
+# repurposed for Dynatrace ActiveGate by Michael Villiger
+# 01/02/2018
+
+#############
+# DYNATRACE #
+#############
+
+# DYNATRACE_DOWNLOAD_URL should be placed in terraform.tfvars along with security/region information
+# URL is obtained from the ActiveGate deployment page or created programmatically in the following format:
+# Dynatrace URL for saas tenants is https://<environmentid>.live.dynatrace.com/api/v1/deployment/installer/gateway/unix/latest?Api-Token=<API_TOKEN>&arch=x86&flavor=default
+# Dynatrace URL for sprint tenents is https://<environmentid>.sprint.dynatracelabs.com/api/v1/deployment/installer/gateway/unix/latest?Api-Token=<API_TOKEN>&arch=x86&flavor=default
+# Dynatrace URL for Managed tenants is https://<YourDynatraceServerHost:Port>/e/<environmentID>/api/v1/deployment/installer/gateway/unix/latest?Api-Token=<API_TOKEN>&arch=x86&flavor=default
+variable "DYNATRACE_DOWNLOAD_URLS" {
+	type = "list"
+}
+
+#######
+# AWS #
+#######
+
+# all access keys, ssh keys, and region information should be in the terraform.tfvars file that is created before terraform apply
+variable "AWS_ACCESS_KEY" {}
+
+variable "AWS_SECRET_KEY" {}
+
+variable "AWS_REGION" {}
+
+variable "AWS_KEYPAIR_NAME" {}
+
+variable "AWS_PRIVATE_KEY" {}
+
+variable "emailtag" {}
+
+#specify the root volume size. default is 20GB
+variable "ROOT_VOLUME_SIZE" {
+default = 10
+}
+
+#here are the latest Ubuntu 16.04 AMI IDs already listed for each region 
+variable "AMIS"{
+	type = "map"
+	default = {
+		us-east-1 = "ami-059eeca93cf09eebd"
+		us-east-2 = "ami-0782e9ee97725263d"
+		us-west-1 = "ami-0ad16744583f21877"
+		us-west-2 = "ami-0e32ec5bc225539f5"
+		eu-west-1 = "ami-0773391ae604c49a4"
+		eu-west-2 = "ami-061a2d878e5754b62"
+		eu-west-3 = "ami-075b44448d2276521"
+		sa-east-1 = "ami-0318cb6e2f90d688b"
+		eu-central-1 = "ami-086a09d5b9fa35dc7"
+		ap-northeast-1 = "ami-06c43a7df16e8213c"
+		ap-northeast-2 = "ami-0e0f4ff1154834540"
+		ap-northeast-3 = "ami-0e8fa3dc3fa30d1da"
+		ap-south-1 = "ami-04ea996e7a3e7ad6b"
+		ap-southeast-1 = "ami-0eb1f21bbd66347fe"
+		ap-southeast-2 = "ami-0789a5fb42dcccc10"
+		ca-central-1 = "ami-0f2cb2729acf8f494"
+	}
+}
+
+variable "DYNATRACE_SIZING" {
+#check the "AWS_INSTANCE_TYPE" values in the next block for the available options
+#please keep in mind that even using the trial sizing you will incur in expenses
+#you can comment the following line to be prompted for the dynatrace-sizing
+	default = "default"
+}
+
+variable "AWS_INSTANCE_TYPE" {
+#the available Dynatrace Sizings available are listed here:
+#https://help.dynatrace.com/dynatrace-managed/introduction/what-are-the-hardware-and-software-requirements/
+#since Dynatrace Managed it's more memory consuming you can opt to choose the memory optimized EC2 instance types
+#by specifying trial-memory, small-memory, medium-memory or large-memory
+	type = "map"
+	default = {
+		default = "t3.medium" 
+		minimum = "t3.small"
+		large = "t3.large"
+		xlarge = "t3.xlarge"
+		t2default = "t2.medium"
+		t2minmum = "t2.small"
+	}
+}
+
+
+
