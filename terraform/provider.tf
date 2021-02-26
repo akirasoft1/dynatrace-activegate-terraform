830a44c516163b631cfe26876025318233b090c1
akirasoft <akirasoft@gmail.com>
2021-02-26 07:09:10 -0600

    another tf 0.14 upgrade

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -15,7 +15,8 @@
 # https://www.terraform.io/docs/configuration/providers.html
 
 provider "aws" {
-	access_key = "${var.AWS_ACCESS_KEY}"
-	secret_key = "${var.AWS_SECRET_KEY}"
-	region = "${var.AWS_REGION}"
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+  region     = var.AWS_REGION
 }
+
